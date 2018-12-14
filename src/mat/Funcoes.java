04de533119d920be12ac5ae637bb7d24fb512fda
E. C. Corrêa <ecmac@cin.ufpe.br>
2018-12-13 22:22:12 -0300

    Uso static de Funcoes

--- src/mat/Funcoes.java
+++ src/mat/Funcoes.java
@@ -2,7 +2,7 @@ package mat;
 
 public class Funcoes {
 	
-	public Ponto interp (Ponto a, Ponto b, float t) {
+	public static Ponto interp (Ponto a, Ponto b, float t) {
 		return (a.mult(1-t)).adi(b.mult(t));
 	}
 
--- src/mat/Testes.java
+++ src/mat/Testes.java
@@ -7,8 +7,6 @@ public class Testes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Funcoes f = new Funcoes();
-		
 		Scanner in = new Scanner(System.in);
 		float Ax = in.nextFloat();
 		float Ay = in.nextFloat();
@@ -22,7 +20,7 @@ public class Testes {
 		System.out.println(A.toString());
 		System.out.println(B.toString());
 		
-		Ponto P = f.interp(A, B, t);
+		Ponto P = Funcoes.interp(A, B, t);
 		
 		System.out.println(P.toString());
 		
